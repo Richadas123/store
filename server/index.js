01cafa2de40331c2095175ca7c825d4c667cba54
Richadas123 <richadassrsvm@gmail.com>
2025-06-14 12:21:18 +0530

    change  with .env

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const morgan=require("morgan");
 
 //middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://store-1-ddok.onrender.com'],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
